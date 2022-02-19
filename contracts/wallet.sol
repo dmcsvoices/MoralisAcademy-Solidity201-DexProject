1c0aa8575022217d0657e50eafd1862367514ca7
Darren Santos <dmcs.voices@gmail.com>
2022-02-18 20:54:53 -0800

    working on Wallet.sol

--- contracts/wallet.sol
+++ contracts/wallet.sol
@@ -1,10 +1,31 @@
-pragma solidity >=0.6.0 <0.8.0
+pragma solidity >=0.6.0 <0.8.0;
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Wallet {
 
+    struct Token {
+        bytes32 ticker;
+        address tokenAddress;
+    }
+
+    mapping(bytes32 => Token) public tokenMapping;
+    bytes32[] public tokenList;
+
     mapping(address => mapping(byte32 => uint256)) public balances;
 
+    function addToken(bytes32 ticker, address tokenAddress) external {
+        tokenMapping[ticker] = Token(ticker, tokenAddress);
+        tokenList.push(ticker);
+    }
 
+    function deposit(uint amount, bytes32 ticker) external {
 
+    }
+
+    function withdraw(uint amount, bytes32 ticker) external {
+        balances[msg.sender][ticker] =  balances[msg.sender][ticker].sub(amount);
+        IERC20(tokenMapping[ticker].tokenAddress).transfer(msg.sender, amount);
+    }
 
 }
